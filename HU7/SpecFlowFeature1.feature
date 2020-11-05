b4a8064328cccaa4b74de0607ed4b2744b182989
Mariia <mary.yarovikova@gmail.com>
2020-11-05 10:33:04 +0100

    Projektdateien hinzufügen.

---
+++ HU7/SpecFlowFeature1.feature
@@ -0,0 +1,18 @@
+﻿Feature: Rechner
+Rechner für trigonometrische Funktionen
+
+@mytag
+Scenario: sinus errechnen
+	Given the number is Pi
+	When the sinus of number
+	Then the result should be 0
+
+Scenario: cosinus errechnen
+	Given the number is Pi
+	When the cosinus of number
+	Then the result should be -1
+
+Scenario: tangens errechnen
+	Given the number is Pi
+	When the tangens of number
+	Then the result should be 0
---
+++ HU7/SpecFlowFeature1Steps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace HU7
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double number, result;        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = result;
+            Assert.Equal(expected, actual, 5);
+        }
+        [Given(@"the number is Pi")]
+        public void GivenTheNumberIsPi()
+        {
+            number = Math.PI;
+        }
+
+        [When(@"the sinus of number")]
+        public void WhenTheSinusOfNumber()
+        {
+            result = Math.Sin(number);
+        }
+        [When(@"the cosinus of number")]
+        public void WhenTheCosinusOfNumber()
+        {
+            result = Math.Cos(number);
+        }
+        [When(@"the tangens of number")]
+        public void WhenTheTangensOfNumber()
+        {
+            result = Math.Tan(number);
+        }
+    }
+}
